89e0aa31e6d7dc7eba0060559bfc1be81ed40396
leobaltazor <leo.baltazor@gmail.com>
2018-06-04 07:22:21 +0300

    100% +adaptive

--- app/js/common.js
+++ app/js/common.js
@@ -1,20 +1,20 @@
 $(function() {
-	$(window).on("load", function() {
-	  $(".servic-description").mCustomScrollbar({
-		theme:"my-theme"
-	  });
-	});
-	// $(document).ready(function(){
-	// 	$(".owl-carousel").owlCarousel({
-	// 		loop:true,
-	// 		lazyLoad: true,
-	// 		dot:false,
-	// 		navText: ['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
-	// 		items:1,
-	// 		autoHeight:true,
-	// 		nav:true
-	// 	});
+	// $(window).on("load", function() {
+	//   $(".servic-description").mCustomScrollbar({
+	// 	theme:"my-theme"
 	//   });
+	// });
+	$(document).ready(function(){
+		$(".owl-carousel").owlCarousel({
+			loop:true,
+			lazyLoad: true,
+			dot:false,
+			navText: ['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+			items:1,
+			autoHeight:true,
+			nav:true
+		});
+	  });
   // Custom JS
 });
 
